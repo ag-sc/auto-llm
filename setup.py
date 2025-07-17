e11908eee77e250cc252b6c8cbe4f8f947b052a4
Viju Sudhi <vjusudhi@gmail.com>
2025-07-17 12:31:31 +0200

    chore: re-structured the package

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+from autollm.version import __version__
+
+setuptools.setup(
+    name="AutoLLM",
+    version=__version__,
+    author="Bielefeld University",
+    description="AutoLLM",
+    packages=setuptools.find_packages(exclude=["src*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.8",
+    install_requires=["pydantic==2.5.1", "requests>=2.27.1"],
+)
