8293760408b6da24ee27cc53f420faceef888261
Viju Sudhi <vjusudhi@gmail.com>
2026-06-10 19:08:51 +0200

    refactor: updated NER metrics

--- auto_llm/evaluator/utils.py
+++ auto_llm/evaluator/utils.py
@@ -1,13 +1,114 @@
 import os
 import json
 import logging
+from typing import Any, Dict, List
 
 from lm_eval.config.evaluate_config import EvaluatorConfig
+from lm_eval.loggers.wandb_logger import WandbLogger
+import numpy as np
+import pandas as pd
+
+
+class CustomWandbLogger(WandbLogger):
+    """
+    In wandb, if the task config does not have a metric list, this function errs out.
+    They use ``config["metric_list"]``.
+    """
+
+    def _generate_dataset(self, data: List[Dict[str, Any]], config: Dict[str, Any]) -> pd.DataFrame:
+        """Generate a dataset from evaluation data.
+
+        Args:
+            data (List[Dict[str, Any]]): The data to generate a dataset for.
+            config (Dict[str, Any]): The configuration of the task.
+
+        Returns:
+            pd.DataFrame: A dataframe that is ready to be uploaded to W&B.
+        """
+        ids = [x["doc_id"] for x in data]
+        labels = [x["target"] for x in data]
+        instance = [""] * len(ids)
+        resps = [""] * len(ids)
+        filtered_resps = [""] * len(ids)
+        model_outputs = {}
+
+        # in wandb, if the task config does not have a metric list, this function errs out.
+        # They use ``config["metric_list"]``.
+        metrics_list = config.get("metric_list")
+        if not metrics_list:
+            metrics_list = []
+            for x in data:
+                for metric in x["metrics"]:
+                    if metric not in metrics_list:
+                        metrics_list.append({"metric": metric})
+
+        metrics = {}
+        for metric in metrics_list:
+            metric = metric.get("metric")
+            if metric in ["word_perplexity", "byte_perplexity", "bits_per_byte"]:
+                metrics[f"{metric}_loglikelihood"] = [x[metric][0] for x in data]
+                if metric in ["byte_perplexity", "bits_per_byte"]:
+                    metrics[f"{metric}_bytes"] = [x[metric][1] for x in data]
+                else:
+                    metrics[f"{metric}_words"] = [x[metric][1] for x in data]
+            else:
+                metrics[metric] = [x[metric] for x in data]
+
+        if config["output_type"] == "loglikelihood":
+            instance = [x["arguments"][0][0] for x in data]
+            labels = [x["arguments"][0][1] for x in data]
+            resps = [
+                f"log probability of continuation is {x['resps'][0][0][0]} "
+                + "\n\n"
+                + "continuation will {} generated with greedy sampling".format("not be" if not x["resps"][0][0][1] else "be")
+                for x in data
+            ]
+            filtered_resps = [
+                f"log probability of continuation is {x['filtered_resps'][0][0]} "
+                + "\n\n"
+                + "continuation will {} generated with greedy sampling".format("not be" if not x["filtered_resps"][0][1] else "be")
+                for x in data
+            ]
+        elif config["output_type"] == "multiple_choice":
+            instance = [x["arguments"][0][0] for x in data]
+            choices = ["\n".join([f"{idx}. {y[1]}" for idx, y in enumerate(x["arguments"])]) for x in data]
+            resps = [np.argmax([n[0][0] for n in x["resps"]]) for x in data]
+            filtered_resps = [np.argmax([n[0] for n in x["filtered_resps"]]) for x in data]
+        elif config["output_type"] == "loglikelihood_rolling":
+            instance = [x["arguments"][0][0] for x in data]
+            resps = [x["resps"][0][0] for x in data]
+            filtered_resps = [x["filtered_resps"][0] for x in data]
+        elif config["output_type"] == "generate_until":
+            instance = [x["arguments"][0][0] for x in data]
+            resps = [x["resps"][0][0] for x in data]
+            filtered_resps = [x["filtered_resps"][0] for x in data]
+
+        model_outputs["raw_predictions"] = resps
+        model_outputs["filtered_predictions"] = filtered_resps
+
+        df_data = {
+            "id": ids,
+            "data": instance,
+        }
+        if config["output_type"] == "multiple_choice":
+            df_data["choices"] = choices
+
+        tmp_data = {
+            "input_len": [len(x) for x in instance],
+            "labels": labels,
+            "output_type": config["output_type"],
+        }
+        df_data.update(tmp_data)
+        df_data.update(model_outputs)
+        df_data.update(metrics)
+
+        return pd.DataFrame(df_data)
 
 
 def run_lm_eval_harness(config_path: str):
     """
-    This is taken as is from lm_eval/_cli/run.py (version: 0.4.12). We pass the `EvaluatorConfig` object directly to this function,
+    This is taken as is from lm_eval/_cli/run.py (version: 0.4.12).
+    We pass the `EvaluatorConfig` object directly to this function,
     so we skip the CLI argument parsing and validation steps.
     """
     cfg = EvaluatorConfig.from_config(config_path)
@@ -19,7 +120,7 @@ def run_lm_eval_harness(config_path: str):
 
     # Set up logging
     if cfg.wandb_args:
-        wandb_logger = WandbLogger(cfg.wandb_args, cfg.wandb_config_args)
+        wandb_logger = CustomWandbLogger(cfg.wandb_args, cfg.wandb_config_args)
         wandb_logger.run.log_artifact(config_path, name="config")
     if cfg.trackio_args:
         trackio_logger = TrackioLogger(cfg.trackio_args)
--- tasks/sequence_to_structured_output/ner/utils.py
+++ tasks/sequence_to_structured_output/ner/utils.py
@@ -40,186 +40,6 @@ def parse_dict(text: str) -> Union[Dict, None]:
             return None
 
 
-def process_results(doc: Dict[str, Any], result: List[str]) -> Dict[str, float]:
-    """Function to compute the metrics given the input and the generated response."""
-    print("Response:\n", result)
-
-    expected_entities_dict = doc.get("output_text", {})
-
-    predicted_response = result[0] if result else ""
-    extracted_text = clean_and_extract_json(text=predicted_response)
-    predicted_response_dict = parse_dict(text=extracted_text)
-
-    """
-    `per_label_scores`: Dict[str, Dict[str, float]] --> Metric values for each label per sample
-        {
-            "label-name": 
-                {
-                    "metric-1-name": metric-1-value,
-                    "metric-2-name": metric-2-value,
-                    ...
-                }
-        }
-
-    `per_sample_scores`: Dict[str, float] --> Metric values for each sample
-        {
-            "metric-1-name": metric-1-value,
-            "metric-2-name": metric-2-value,
-            ...
-        }
-    """
-
-    per_label_scores: Dict[str, Dict[str, float]] = get_per_label_scores(expected_entities_dict=expected_entities_dict, predicted_response_dict=predicted_response_dict)
-    per_sample_scores: Dict[str, float] = get_per_sample_scores(expected_entities_dict=expected_entities_dict, predicted_response_dict=predicted_response_dict)
-
-    consolidated_metrics_dict = {}
-    for key, value in per_label_scores.items():
-        for metric_name, metric_value in value.items():
-            consolidated_metrics_dict[f"{metric_name}_{key}"] = metric_value
-
-    for metric_name, metric_value in per_sample_scores.items():
-        consolidated_metrics_dict[f"{metric_name}_all_labels"] = metric_value
-
-    return consolidated_metrics_dict
-
-
-def get_per_sample_scores(expected_entities_dict: Dict[str, List], predicted_response_dict: Dict[str, List]):
-    exact_match_score = 0.0
-    partial_match_score = 0.0
-    fuzzy_match_score = 0.0
-    f1_score = 0.0
-
-    if not isinstance(predicted_response_dict, dict):
-        print("Cannot parse response, cannot compute score. Keeping scores 0")
-        print("----------------------------")
-        return {
-            "exact_match": 0.0,
-            "partial_match": 0.0,
-            "fuzzy_match": 0.0,
-            "f1_score": 0.0,
-        }
-
-    num_entity_keys_with_values = 0
-    for key, expected_value in expected_entities_dict.items():
-        if not isinstance(expected_value, list):
-            expected_value = [str(expected_value)] if expected_value else []
-
-        num_entity_keys_with_values += 1
-
-        # TODO: Safe default is an empty list
-        predicted_value = predicted_response_dict.get(key, [])
-        if not isinstance(predicted_value, list):
-            predicted_value = [str(predicted_value)] if predicted_value else []
-
-        print(f"\nKey: {key}, Expected: {expected_value}, Predicted: {predicted_value}")
-
-        # 1. Exact match between the Lists
-        exact_match = get_exact_match(expected_value, predicted_value)
-        exact_match_score += exact_match
-
-        # 2. Partial match (Exact token containment)
-        partial_match = get_partial_match(expected_value, predicted_value)
-        partial_match_score += partial_match
-
-        # 3. Fuzzy ratio match
-        fuzzy_match = get_fuzzy_match(expected_value, predicted_value)
-        fuzzy_match_score += fuzzy_match
-
-        # 4. F1 Score
-        f1 = get_f1_score(expected_value, predicted_value)
-        f1_score += f1
-
-        print(f"Exact Match: {exact_match}")
-        print(f"Partial Match: {partial_match}")
-        print(f"Fuzzy Match: {fuzzy_match}")
-        print(f"F1 Score: {f1}")
-        print("---------")
-
-    # Guard against completely empty configurations
-    if num_entity_keys_with_values > 0:
-        exact_match_score /= num_entity_keys_with_values
-        partial_match_score /= num_entity_keys_with_values
-        fuzzy_match_score /= num_entity_keys_with_values
-        f1_score /= num_entity_keys_with_values
-
-    print("Final exact_match_score:", exact_match_score)
-    print("Final partial_match_score:", partial_match_score)
-    print("Final fuzzy_match_score:", fuzzy_match_score)
-    print("Final f1_score:", f1_score)
-    print("----------------------------")
-
-    return {
-        "exact_match": exact_match_score,
-        "partial_match": partial_match_score,
-        "fuzzy_match": fuzzy_match_score,
-        "f1_score": f1_score,
-    }
-
-
-def get_per_label_scores(expected_entities_dict: Dict[str, List], predicted_response_dict: Dict[str, List]) -> Dict[str, Dict[str, float]]:
-    scores_dict = {
-        "exact_match": 0,
-        "partial_match": 0,
-        "fuzzy_match": 0,
-        "f1_score": 0,
-    }
-
-    per_label_scores_dict = {label: scores_dict for label in list(expected_entities_dict.keys())}
-
-    if not isinstance(predicted_response_dict, dict):
-        print("Cannot parse response, cannot compute score. Keeping scores 0")
-        print("----------------------------")
-        return per_label_scores_dict
-
-    num_entity_keys_with_values = 0
-    for key, expected_value in expected_entities_dict.items():
-        if not isinstance(expected_value, list):
-            expected_value = [str(expected_value)] if expected_value else []
-
-        num_entity_keys_with_values += 1
-
-        # TODO: Safe default is an empty list
-        predicted_value = predicted_response_dict.get(key, [])
-        if not isinstance(predicted_value, list):
-            predicted_value = [str(predicted_value)] if predicted_value else []
-
-        print(f"\nKey: {key}, Expected: {expected_value}, Predicted: {predicted_value}")
-
-        # 1. Exact match between the Lists
-        exact_match = get_exact_match(expected_value, predicted_value)
-        per_label_scores_dict[key]["exact_match"] += exact_match
-
-        # 2. Partial match (Exact token containment)
-        partial_match = get_partial_match(expected_value, predicted_value)
-        per_label_scores_dict[key]["partial_match"] += partial_match
-
-        # 3. Fuzzy ratio match
-        fuzzy_match = get_fuzzy_match(expected_value, predicted_value)
-        per_label_scores_dict[key]["fuzzy_match"] += fuzzy_match
-
-        # 4. F1 Score
-        f1_score = get_f1_score(expected_value, predicted_value)
-        per_label_scores_dict[key]["f1_score"] += f1_score
-
-        print(f"Exact Match: {exact_match}")
-        print(f"Partial Match: {partial_match}")
-        print(f"Fuzzy Match: {fuzzy_match}")
-        print(f"F1 Score: {f1_score}")
-        print("---------")
-
-    for key, expected_value in expected_entities_dict.items():
-        # Guard against completely empty configurations
-        if num_entity_keys_with_values > 0:
-            per_label_scores_dict[key]["exact_match"] /= num_entity_keys_with_values
-            per_label_scores_dict[key]["partial_match"] /= num_entity_keys_with_values
-            per_label_scores_dict[key]["fuzzy_match"] /= num_entity_keys_with_values
-            per_label_scores_dict[key]["f1_score"] /= num_entity_keys_with_values
-
-    print("Final scores", per_label_scores_dict)
-
-    return per_label_scores_dict
-
-
 def get_f1_score(expected_value, predicted_value):
     exp_set = set(expected_value)
     pred_set = set(predicted_value)
@@ -266,3 +86,159 @@ def get_partial_match(expected_value, predicted_value):
 def get_exact_match(expected_value, predicted_value):
     exact_match_score = set(expected_value) == set(predicted_value)
     return float(exact_match_score)
+
+
+def process_results(doc: Dict[str, Any], result: List[str]) -> Dict[str, float]:
+    """Function to compute the metrics given the input and the generated response."""
+    expected_entities_dict = doc.get("output_text", {})
+
+    predicted_response = result[0] if result else ""
+    extracted_text = clean_and_extract_json(text=predicted_response)
+    predicted_response_dict = parse_dict(text=extracted_text)
+
+    """
+    `per_label_scores`: Dict[str, Dict[str, float]] --> Metric values for each label per sample
+        {
+            "label-name": 
+                {
+                    "metric-1-name": metric-1-value,
+                    "metric-2-name": metric-2-value,
+                    ...
+                }
+        }
+
+    `per_sample_scores`: Dict[str, float] --> Metric values for each sample
+        {
+            "metric-1-name": metric-1-value,
+            "metric-2-name": metric-2-value,
+            ...
+        }
+    """
+
+    per_label_scores: Dict[str, Dict[str, float]] = get_per_label_scores(expected_entities_dict=expected_entities_dict, predicted_response_dict=predicted_response_dict)
+    per_sample_scores: Dict[str, float] = get_per_sample_scores(expected_entities_dict=expected_entities_dict, predicted_response_dict=predicted_response_dict)
+
+    consolidated_metrics_dict = {}
+    for key, value in per_label_scores.items():
+        for metric_name, metric_value in value.items():
+            consolidated_metrics_dict[f"{metric_name}_{key}"] = metric_value
+
+    for metric_name, metric_value in per_sample_scores.items():
+        consolidated_metrics_dict[f"{metric_name}_all_labels"] = metric_value
+
+    print("Input:", doc.get("input_text", {}))
+    print("Expected Output:", expected_entities_dict)
+    print("Generated Output:", predicted_response_dict)
+    print("Consolidated Metrics:", consolidated_metrics_dict)
+
+    return consolidated_metrics_dict
+
+
+def get_per_sample_scores(expected_entities_dict: Dict[str, List], predicted_response_dict: Dict[str, List]):
+    exact_match_score = 0.0
+    partial_match_score = 0.0
+    fuzzy_match_score = 0.0
+    f1_score = 0.0
+
+    if not isinstance(predicted_response_dict, dict):
+        print("Cannot parse response, cannot compute score. Keeping scores 0")
+        print("----------------------------")
+        return {
+            "exact_match": 0.0,
+            "partial_match": 0.0,
+            "fuzzy_match": 0.0,
+            "f1_score": 0.0,
+        }
+
+    num_entity_keys_with_values = 0
+    for key, expected_value in expected_entities_dict.items():
+        if not isinstance(expected_value, list):
+            expected_value = [str(expected_value)] if expected_value else []
+
+        num_entity_keys_with_values += 1
+
+        predicted_value = predicted_response_dict.get(key, [])
+        if not isinstance(predicted_value, list):
+            predicted_value = [str(predicted_value)] if predicted_value else []
+
+        # 1. Exact match between the Lists
+        exact_match = get_exact_match(expected_value, predicted_value)
+        exact_match_score += exact_match
+
+        # 2. Partial match (Exact token containment)
+        partial_match = get_partial_match(expected_value, predicted_value)
+        partial_match_score += partial_match
+
+        # 3. Fuzzy ratio match
+        fuzzy_match = get_fuzzy_match(expected_value, predicted_value)
+        fuzzy_match_score += fuzzy_match
+
+        # 4. F1 Score
+        f1 = get_f1_score(expected_value, predicted_value)
+        f1_score += f1
+
+    if num_entity_keys_with_values > 0:
+        exact_match_score /= num_entity_keys_with_values
+        partial_match_score /= num_entity_keys_with_values
+        fuzzy_match_score /= num_entity_keys_with_values
+        f1_score /= num_entity_keys_with_values
+
+    return {
+        "exact_match": exact_match_score,
+        "partial_match": partial_match_score,
+        "fuzzy_match": fuzzy_match_score,
+        "f1_score": f1_score,
+    }
+
+
+def get_per_label_scores(expected_entities_dict: Dict[str, List], predicted_response_dict: Dict[str, List]) -> Dict[str, Dict[str, float]]:
+    scores_dict = {
+        "exact_match": 0,
+        "partial_match": 0,
+        "fuzzy_match": 0,
+        "f1_score": 0,
+    }
+
+    per_label_scores_dict = {label: scores_dict for label in list(expected_entities_dict.keys())}
+
+    if not isinstance(predicted_response_dict, dict):
+        print("Cannot parse response, cannot compute score. Keeping scores 0")
+        print("----------------------------")
+        return per_label_scores_dict
+
+    num_entity_keys_with_values = 0
+    for key, expected_value in expected_entities_dict.items():
+        if not isinstance(expected_value, list):
+            expected_value = [str(expected_value)] if expected_value else []
+
+        num_entity_keys_with_values += 1
+
+        predicted_value = predicted_response_dict.get(key, [])
+        if not isinstance(predicted_value, list):
+            predicted_value = [str(predicted_value)] if predicted_value else []
+
+        # 1. Exact match between the Lists
+        exact_match = get_exact_match(expected_value, predicted_value)
+        per_label_scores_dict[key]["exact_match"] += exact_match
+
+        # 2. Partial match (Exact token containment)
+        partial_match = get_partial_match(expected_value, predicted_value)
+        per_label_scores_dict[key]["partial_match"] += partial_match
+
+        # 3. Fuzzy ratio match
+        fuzzy_match = get_fuzzy_match(expected_value, predicted_value)
+        per_label_scores_dict[key]["fuzzy_match"] += fuzzy_match
+
+        # 4. F1 Score
+        f1_score = get_f1_score(expected_value, predicted_value)
+        per_label_scores_dict[key]["f1_score"] += f1_score
+
+    for key, expected_value in expected_entities_dict.items():
+        # Guard against completely empty configurations
+        if num_entity_keys_with_values > 0:
+            per_label_scores_dict[key]["exact_match"] /= num_entity_keys_with_values
+            per_label_scores_dict[key]["partial_match"] /= num_entity_keys_with_values
+            per_label_scores_dict[key]["fuzzy_match"] /= num_entity_keys_with_values
+            per_label_scores_dict[key]["f1_score"] /= num_entity_keys_with_values
+
+    return per_label_scores_dict
