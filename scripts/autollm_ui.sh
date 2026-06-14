c7956cb9118a0d952282de49d70e72121d3e2bcb
Viju Sudhi <viju.sudhi@uni-bielefeld.de>
2026-06-14 11:46:30 +0200

    fix: specify ports for reflex run

--- scripts/autollm_ui.sh
+++ scripts/autollm_ui.sh
@@ -1,8 +1,7 @@
 cd ui
-fuser -k 3000/tcp
-fuser -k 8000/tcp
+fuser -k 3000/tcp && fuser -k 8000/tcp
 
 ENV_VARIABLES_PATH=/home/ubuntu/env.sh
 source $ENV_VARIABLES_PATH
 
-reflex run
+reflex run --frontend-port 3000 --backend-port 8000
