a277197ba1ced037a86b1acb01bc7321cd1a8f4e
andreea <andreea.scrob@studio.unibo.it>
2026-06-28 17:31:48 +0200

    fix: update WANDB training project name to 'open-medical-llm-train-energy'

--- auto_llm/registry/tracker_registry.py
+++ auto_llm/registry/tracker_registry.py
@@ -1,4 +1,5 @@
-WANDB_TRAIN_PROJECT = "open-medical-llm-QLoRa"
+WANDB_TRAIN_PROJECT = "open-medical-llm-train-energy"
 
 WANDB_TRAIN_REPORT_URL = "https://api.wandb.ai/links/llm4kmu/v9qfir8d"
 WANDB_EVAL_REPORT_URL = "https://api.wandb.ai/links/llm4kmu/rm3d6va1"
+open-medical-llm-
